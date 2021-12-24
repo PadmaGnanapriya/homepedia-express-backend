e28bc8f3f362945045e1aa95d533baaddb289bb2
PadmaGnanapriya <padmaisuru@gmail.com>
2021-12-24 20:42:11 +0530

    Create error handler middleware.

---
+++ src/middleware/errorHandler.ts
@@ -0,0 +1,25 @@
+/**
+ *
+ * @param {any} error
+ * @param {any} message
+ * @param {any} data
+ * @returns {{status: string}}
+ */
+export function createErrorResponse(error: any) {
+  const json: any = {
+    status: 'error',
+    message: 'Internal server error'
+  };
+  return json;
+}
+
+/**
+ *
+ * @returns {Function}
+ */
+export default function errorHandler() {
+  return (err: any, req: any, res: any, next: any) => {
+    const statusCode = err.statusCode || 500;
+    return res.status(statusCode).send(createErrorResponse(err));
+  };
+}
